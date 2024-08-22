ee3ff473dabb0b484d515765a8118ea7db7e0438
jchillah <jchillah@gmail.com>
2024-08-22 09:55:47 +0200

    Update check_palindrome.dart

--- lib/check_palindrome.dart
+++ lib/check_palindrome.dart
@@ -1,4 +1,4 @@
-bool isPalindrome(String s) {
-  s = s.toLowerCase().replaceAll(' ', '');
-  return s == s.split('').reversed.join('');
+bool isPalindrome(String input) {
+  input = input.toLowerCase().replaceAll(' ', '');
+  return input == input.split('').reversed.join('');
 }
