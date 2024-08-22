51b69c745cbcba3a64c054c3b16bceab5e95a1af
jchillah <jchillah@gmail.com>
2024-08-22 10:09:00 +0200

    Update check_palindrome.dart

--- bin/check_palindrome.dart
+++ bin/check_palindrome.dart
@@ -2,6 +2,8 @@ import 'package:check_palindrome/check_palindrome.dart';
 
 void main() {
   print(isPalindrome("A man a plan a canal Panama"));
-  print(istPalindrom('otto'));  
-  print(istPalindrom('palindrom'));
+  print(isPalindrom('otto'));  
+  print(isPalindrom('palindrom'));
+  print(istPalindrom('anna'));
+  print(istPalindrom('hello')); 
 }
