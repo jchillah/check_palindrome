9ceecd807a148419da05c8d39105906846fe3f55
jchillah <jchillah@gmail.com>
2024-08-22 10:06:45 +0200

    Update check_palindrome.dart

--- lib/check_palindrome.dart
+++ lib/check_palindrome.dart
@@ -2,3 +2,13 @@ bool isPalindrome(String input) {
   input = input.toLowerCase().replaceAll(' ', '');
   return input == input.split('').reversed.join('');
 }
+
+bool istPalindrom(String text) {
+  String wordReversed = text.split('').reversed.join('');
+  return text == wordReversed;
+}
+
+void main() {
+  print(istPalindrom('otto'));  
+  print(istPalindrom('palindrom'));  
+}
